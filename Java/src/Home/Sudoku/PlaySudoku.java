0b53cdf1bb080c1475a2bc23683fc28ba86052e1
KaydenWeatherford <143819214+KaydenWeatherford@users.noreply.github.com>
2025-05-09 20:13:44 +0000

    Started WIP for the game controls

--- Java/src/Home/Sudoku/PlaySudoku.java
+++ Java/src/Home/Sudoku/PlaySudoku.java
@@ -3,9 +3,8 @@ import Home.Sudoku.Sudoku;
 
 public class PlaySudoku{
     public static void main(String[] args){
-        Sudoku s = new Sudoku();
-        s.generateBoard(3);  // 3 = Hard
-        s.displayBoard();
+        gMenu({ "New Game", " "});
+        
         
 
     }
--- Java/src/Home/Sudoku/gMenu.java
+++ Java/src/Home/Sudoku/gMenu.java
@@ -1,5 +1,38 @@
 package Home.Sudoku;
 
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class gMenu{
+    
+    private LinkedHashMap<Integer, String> options;
+    
+    public gMenu(String[] args){
+        for (int i = 1; i <= args.length; i++){
+            options.put(i, args[i-1]);
+        }
+        options.put(0, "Exit");
+    }
+    
+    public void disOptions(){
+
+        for (int i = 1; i < options.size(); i++){
+            System.out.println(options.get(i) + ") - " + findKey(options.get(i)));
+        }
+        System.out.println("\n" + options.get(0) + ") - " + findKey(options.get(0)));
+    }
+
+    private int findKey(String targetValue){
+        int key = -1;
+        for (Map.Entry<Integer, String> x : options.entrySet()) {
+            if (x.getValue().equals(targetValue)) {
+                key = x.getKey();
+                break;
+            }
+        }
+        return key;
+    }
+
     
 }
